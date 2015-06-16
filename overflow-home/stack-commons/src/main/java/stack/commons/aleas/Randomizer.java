83d1e6983f398618dd43178328b135d8fb95236d
alainlompo <alain.lompo@gmail.com>
2015-06-16 02:59:42 +0100

    Enriching the Randomizer
    Added a few methods to the Randomizer

--- overflow-home/stack-commons/src/main/java/stack/commons/aleas/Randomizer.java
+++ overflow-home/stack-commons/src/main/java/stack/commons/aleas/Randomizer.java
@@ -13,6 +13,10 @@ public  class Randomizer {
 		
 	}
 	
+	/**
+	* This method will randomly pick up one object among all provided objects
+	*
+	*/
 	public static Object oneOf(Object...objects) {
 		
 		if (null == objects) {			
@@ -22,4 +26,22 @@ public  class Randomizer {
 		}
 			
 	}
+	
+	/**
+	* Will randomly provide a positive integer lesser that maxValue
+	*
+	*/
+	public static int integerBetweenZeroAnd(int maxValue) {
+		return randomHelper.nextInt(maxValue);
+	}
+	
+	/**
+	* Will randomly provide a positive double lesser that maxValue
+	*
+	*/
+	public static double positiveDoubleLesserThan(double maxValue) {
+		return randomHelper.nextDouble() * maxValue;
+	}
+	
+	
 }
